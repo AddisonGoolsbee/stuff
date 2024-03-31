d630d30472fa378b703382a651763c50136151ca
Addison Goolsbee <addisongoolsbee@gmail.com>
2024-03-30 22:03:17 -0400

    Create matrix multiply (used for a pset in 3D spacial modeling)

---
+++ matrixMultiply.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+import numpy as np
+
+A = np.array([[2, 5], [1, 9]])
+B = np.array([[6, 2], [2, 8]])
+C = np.array([[0, 1], [4, 7]])
+
+ABCA = A @ B @ C @ A
+BACA = B @ A @ C @ A
+CABA = C @ A @ B @ A
+CBAA = C @ B @ A @ A
+AABC = A @ A @ B @ C
+ABAC = A @ B @ A @ C
+ACAB = A @ C @ A @ B
+ACBA = A @ C @ B @ A
+
+res = ABCA - BACA - CABA + CBAA - AABC + ABAC + ACAB - ACBA
+print(res)
