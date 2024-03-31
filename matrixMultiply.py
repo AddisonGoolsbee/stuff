75ca2a0cc1bb92be8ea4c9947d6c745f5c8624be
Addison Goolsbee <addisongoolsbee@gmail.com>
2024-03-30 22:38:50 -0400

    Add another function to matrixMultiply

--- matrixMultiply.py
+++ matrixMultiply.py
@@ -1,18 +1,28 @@
 # Multipy some hard-coded matrices. Used for a problem set in the class 3D Spacial Modeling and Computing
 import numpy as np
 
-A = np.array([[2, 5], [1, 9]])
-B = np.array([[6, 2], [2, 8]])
-C = np.array([[0, 1], [4, 7]])
+def problem_2():
+    A = np.array([[2, 5], [1, 9]])
+    B = np.array([[6, 2], [2, 8]])
+    C = np.array([[0, 1], [4, 7]])
 
-ABCA = A @ B @ C @ A
-BACA = B @ A @ C @ A
-CABA = C @ A @ B @ A
-CBAA = C @ B @ A @ A
-AABC = A @ A @ B @ C
-ABAC = A @ B @ A @ C
-ACAB = A @ C @ A @ B
-ACBA = A @ C @ B @ A
+    ABCA = A @ B @ C @ A
+    BACA = B @ A @ C @ A
+    CABA = C @ A @ B @ A
+    CBAA = C @ B @ A @ A
+    AABC = A @ A @ B @ C
+    ABAC = A @ B @ A @ C
+    ACAB = A @ C @ A @ B
+    ACBA = A @ C @ B @ A
 
-res = ABCA - BACA - CABA + CBAA - AABC + ABAC + ACAB - ACBA
-print(res)
+    res = ABCA - BACA - CABA + CBAA - AABC + ABAC + ACAB - ACBA
+    print(res)
+
+def problem_6():
+    T1 = np.array([[0,1,-2],[-1,0,1],[0,0,1]])
+    T2 = np.array([[0,1,3],[-1,0,4],[0,0,1]])
+    print(T1 @ T2)
+
+problem_6()
+
+
